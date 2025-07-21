8f1f9242b365e788b86f1b8fef876061d0ee8058
bansky-cl <cxy1365@163.com>
2025-07-21 15:20:55 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime as dt
 import requests
 import json
 import arxiv
@@ -7,7 +7,7 @@ from arxiv import UnexpectedEmptyPageError
 import matplotlib.pyplot as plt
 from pathlib import Path
 from collections import Counter, defaultdict
-from datetime import dt
+from datetime import datetime
 
 
 
@@ -158,7 +158,7 @@ def json_to_md(filename, md_filename,
     @return None
     """
 
-    DateNow = datetime.date.today()
+    DateNow = dt.date.today()
     DateNow = str(DateNow)
     DateNow = DateNow.replace('-', '.')
 
@@ -261,7 +261,7 @@ def json_to_trend(json_file: str | Path, img_file: str | Path) -> None:
         print("no data")
         return
 
-    ym_dates = {dt.strptime(k, "%Y-%m"): k for k in counts}
+    ym_dates = {datetime.strptime(k, "%Y-%m"): k for k in counts}
     sorted_keys = [ym_dates[d] for d in sorted(ym_dates)]
     values = [counts[k] for k in sorted_keys]
     idx_map = {k: i for i, k in enumerate(sorted_keys)}
